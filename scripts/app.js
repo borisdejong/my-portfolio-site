cb692c7e45bc08f988d56ce401c9775206aea3fb
borisdejong <b.a.s.de.jong@student.rug.nl>
2025-06-15 21:30:36 +0200

    Another edit

--- scripts/app.js
+++ scripts/app.js
@@ -3,9 +3,9 @@ import { myCV } from './cv-data.js';
 function renderPersonalInfo() {
   document.getElementById('myName').textContent = myCV.personalInfo.name;
   document.getElementById("subTitle").textContent = myCV.personalInfo.title;
-}document.getElementById('mySummary').textContent = myCV.summary;
-
+}
 
 document.addEventListener('DOMContentLoaded', () => {
+  renderPersonalInfo();
   document.getElementById('current-year').textContent = new Date().getFullYear();
 });
