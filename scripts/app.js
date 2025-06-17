5ac66c9dd3c21e3b08fea78df038e9d51760f2ba
borisdejong <b.a.s.de.jong@student.rug.nl>
2025-06-17 21:37:33 +0200

    More changes

--- scripts/app.js
+++ scripts/app.js
@@ -10,45 +10,48 @@ function renderPersonalInfo(persInfo) {
 
 function renderExperience(experienceArray) {
     experienceArray.forEach((item) => {
-      let experienceItem = document.createElement("div");
+      const experienceItem = document.createElement("div");
       experienceItem.className = "item";
 
-      let experienceHeader = document.createElement("div");
+      const experienceHeader = document.createElement("div");
       experienceHeader.className = "experience-header";
 
-      let experienceDescription = document.createElement("div");
+      const experienceDescription = document.createElement("div");
       experienceDescription.className = "experience-description";
 
-
-
-      let role = document.createElement("h3");
+      const role = document.createElement("h3");
       role.className = "role";
       role.textContent = `${item.title} @ ${item.company}`;
 
-      let period = document.createElement("div");
+      const period = document.createElement("div");
       period.className = "period";
       period.textContent = `${item.duration}`;
 
-      let keywords = document.createElement("div");
-      keywords.className = "keywords";
-      keywords.textContent = `${item.keywords}`;
-
-      let description = document.createElement("ul");
+      const description = document.createElement("ul");
       description.className = "description";
 
       const descriptionItems = item.description.map(test => {
         return `<li>${test}</li>`; 
-      });
+      }).join("");
 
-      console.log(descriptionItems);
+      const keywords = document.createElement("ul");
+      description.className = "description";
+
+      const keywordItems = item.keywords.map(keywordItem => {
+        return `<li>${keywordItem}</li>`; 
+      }).join("");
+
 
       description.innerHTML = descriptionItems;
+      keywords.innerHTML = keywordItems;
 
       experienceHeader.appendChild(role);
       experienceHeader.appendChild(period);
       experienceDescription.appendChild(description);
+      experienceDescription.appendChild(keywords);
       experienceItem.appendChild(experienceHeader);
       experienceItem.appendChild(experienceDescription);
+      
       document.getElementById("experienceList").appendChild(experienceItem);
     });
 }
