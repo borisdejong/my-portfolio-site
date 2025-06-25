74a16a2a48fd05fd86e945bfe067fb0a0aec5730
borisdejong <b.a.s.de.jong@student.rug.nl>
2025-06-25 20:27:22 +0200

    Added highlighting to search functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,11 @@
-console.log("App.js is running!");
-
 import { myCV } from './cv-data.js';
-
 
 function renderPersonalInfo(persInfo) {
   document.getElementById('myName').textContent = persInfo.name;
   document.getElementById("subTitle").textContent = persInfo.title;
 }
 
-function renderExperience(experienceArray) {
+function renderExperience(experienceArray, highlightTerm) {
     experienceArray.forEach((item) => {
       const experienceItem = document.createElement("li");
       experienceItem.className = "item";
@@ -21,22 +18,21 @@ function renderExperience(experienceArray) {
 
       const role = document.createElement("h3");
       role.className = "role";
-      role.textContent = `${item.title} @ ${item.company}`;
+      role.innerHTML = highlightText(`${item.title} @ ${item.company}`, highlightTerm);
 
       const period = document.createElement("div");
       period.className = "period";
-      period.textContent = `${item.duration}`;
-      role.textContent = `${item.title} @ ${item.company}`;
+      period.innerHTML = highlightText(`${item.duration}`, highlightTerm);
 
       const description = document.createElement("p");
       description.className = "description";
-      description.textContent = item.description;
+      description.innerHTML = highlightText(`${item.description}`, highlightTerm);
 
       const keywords = document.createElement("ul");
       keywords.className = "description-list";
 
       const keywordItems = item.keywords.map(keywordItem => {
-        return `<li>${keywordItem}</li>`; 
+        return `<li>${highlightText(keywordItem, highlightTerm)}</li>`; 
       }).join("");
 
       keywords.innerHTML = keywordItems;
@@ -52,7 +48,7 @@ function renderExperience(experienceArray) {
     });
 }
 
-function renderEducation(educationArray) {
+function renderEducation(educationArray, highlightTerm) {
   const educationList = document.getElementById("educationList");
 
   educationArray.forEach((item) => {
@@ -64,17 +60,17 @@ function renderEducation(educationArray) {
 
     const degree = document.createElement("h3");
     degree.className = "degree";
-    degree.textContent = `${item.degree}`;
+    degree.innerHTML = highlightText(`${item.degree}`, highlightTerm);
 
     const institution = document.createElement("div");
     institution.className = "institution";
-    institution.textContent = `${item.institution} - ${item.year}`;
+    institution.innerHTML = highlightText(`${item.institution} - ${item.year}`, highlightTerm);
     
     const keywords = document.createElement("ul");
     keywords.className = "description-list";
 
     const keywordItems = item.keywords.map(keyword => {
-      return `<li>${keyword}</li>`;
+      return `<li>${highlightText(keyword, highlightTerm)}</li>`;
     }).join("");
 
     keywords.innerHTML = keywordItems;
@@ -88,7 +84,7 @@ function renderEducation(educationArray) {
   });
 }
 
-function renderSkills(skillsArray) {
+function renderSkills(skillsArray, highlightTerm) {
   const skillsList = document.getElementById("skillsList");
 
   skillsArray.forEach((skill) => {
@@ -98,11 +94,11 @@ function renderSkills(skillsArray) {
 
     const skillName = document.createElement("span");
     skillName.className = "skill-name";
-    skillName.textContent = `${skill.name} (${skill.type})`;
+    skillName.innerHTML = highlightText(`${skill.name} (${skill.type})`, highlightTerm);
 
     const skillLevel = document.createElement("span");
     skillLevel.className = `skill-level ${skill.level.toLowerCase()}`;
-    skillLevel.textContent = skill.level;
+    skillLevel.innerHTML = highlightText(skill.level, highlightTerm);
 
     skillItem.appendChild(skillName);
     skillItem.appendChild(skillLevel);
@@ -110,7 +106,7 @@ function renderSkills(skillsArray) {
   });
 }
 
-function renderProjects(projectsArray) {
+function renderProjects(projectsArray, highlightTerm) {
   const projectList = document.getElementById("projectList");
 
   projectsArray.forEach((project) => {
@@ -119,17 +115,17 @@ function renderProjects(projectsArray) {
 
     const projectName = document.createElement("h3");
     projectName.className = "project-name";
-    projectName.textContent = project.name;
+    projectName.innerHTML = highlightText(project.name, highlightTerm);
 
     const projectDescription = document.createElement("p");
     projectDescription.className = "project-description";
-    projectDescription.textContent = project.description;
+    projectDescription.innerHTML = highlightText(project.description, highlightTerm);
 
     const technologies = document.createElement("ul");
     technologies.className = "description-list";
 
     const technologyItems = project.technologies.map(tech => {
-      return `<li>${tech}</li>`;
+      return `<li>${highlightText(tech, highlightTerm)}</li>`;
     }).join("");
 
     technologies.innerHTML = technologyItems;
@@ -159,11 +155,11 @@ function renderProjects(projectsArray) {
   });
 }
 
-function highlightText(text, term) {
-    if (!term || typeof text !== 'string') return text;
-
+function highlightText(text, term) { // Takes as input two strings and checks whether text contains terms. If so, add highlight span to term within text and return updated text
+    if (!term || typeof text !== 'string') {
+      return text;
+    }
     const regex = new RegExp(`(${term})`, 'gi');
-
     return text.replace(regex, '<span class="highlight">$&</span>'); // $& inserts the matched text itself
 }
 
@@ -171,7 +167,9 @@ async function performSearch() {
   const searchTerm = searchInput.value.toLowerCase().trim();
   document.getElementById('experienceList').innerHTML = '';
   document.getElementById('educationList').innerHTML = '';
+  document.getElementById('projectList').innerHTML = '';
   document.getElementById('skillsList').innerHTML = '';
+  noResultsMessage.style.display = 'none';
 
   // Display clearSearchButton only if something is entered in the search input field
   if (searchTerm.length > 0) {
@@ -194,19 +192,6 @@ async function performSearch() {
 
       return searchableText.includes(searchTerm);
   });
-
-  const filteredEducation = myCV.education.filter(educationItem => {
-      // Combine all relevant searchable text from the job object into one lowercase string
-      const searchableTextEducation = [
-          educationItem.degree,
-          educationItem.institution,
-          educationItem.year,
-          ...(educationItem.keywords || [])     // Use spread for keywords, handle if missing
-      ].join(' ').toLowerCase(); // Join all parts with a space
-
-      return searchableTextEducation.includes(searchTerm);
-  });
-
   if (filteredExperience.length > 0) {
       totalMatches += filteredExperience.length;
       // Call your rendering function for experience, passing filtered data and searchTerm for highlighting
@@ -220,6 +205,18 @@ async function performSearch() {
       document.getElementById('experience').style.display = 'block';
   }
 
+
+  const filteredEducation = myCV.education.filter(educationItem => {
+      // Combine all relevant searchable text from the job object into one lowercase string
+      const searchableTextEducation = [
+          educationItem.degree,
+          educationItem.institution,
+          educationItem.year,
+          ...(educationItem.keywords || [])     // Use spread for keywords, handle if missing
+      ].join(' ').toLowerCase(); // Join all parts with a space
+
+      return searchableTextEducation.includes(searchTerm);
+  });
   if (filteredEducation.length > 0) {
       totalMatches += filteredEducation.length;
       // Call your rendering function for experience, passing filtered data and searchTerm for highlighting
@@ -231,6 +228,59 @@ async function performSearch() {
       // If no search term, always render the full list
       renderEducation(myCV.education, '');
       document.getElementById('study').style.display = 'block';
+  }
+
+
+  const filteredProjects = myCV.projects.filter(projectItem => {
+      // Combine all relevant searchable text from the job object into one lowercase string
+      const searchableTextProjects = [
+          projectItem.name,
+          projectItem.description,
+          ...(projectItem.technologies || []),
+          projectItem.link,
+          projectItem.githubLink,
+          ...(projectItem.keywords || [])
+      ].join(' ').toLowerCase(); // Join all parts with a space
+
+      return searchableTextProjects.includes(searchTerm);
+  });
+  if (filteredProjects.length > 0) {
+      totalMatches += filteredProjects.length;
+      // Call your rendering function for experience, passing filtered data and searchTerm for highlighting
+      renderProjects(filteredProjects, searchTerm);
+      document.getElementById('projects').style.display = 'block'; // Ensure section is visible
+  } else if (searchTerm) {
+      document.getElementById('projects').style.display = 'none'; // Hide if no matches and there's a search term
+  } else {
+      // If no search term, always render the full list
+      renderProjects(myCV.projects, '');
+      document.getElementById('projects').style.display = 'block';
+  }
+
+
+  const filteredSkills = myCV.skills.filter(skillItem => {
+    // Combine all relevant searchable text from the job object into one lowercase string
+    const searchableTextSkills = [
+        skillItem.name,
+        skillItem.level
+    ].join(' ').toLowerCase(); // Join all parts with a space
+
+    return searchableTextSkills.includes(searchTerm);
+  });
+  if (filteredSkills.length > 0) {
+      totalMatches += filteredSkills.length;
+      renderSkills(filteredSkills, searchTerm);
+      document.getElementById('skills').style.display = 'block'; // Ensure section is visible
+  } else if (searchTerm) {
+      document.getElementById('skills').style.display = 'none'; // Hide if no matches and there's a search term
+  } else {
+      // If no search term, always render the full list
+      renderSkills(myCV.skills, '');
+      document.getElementById('skills').style.display = 'block';
+  }
+
+  if (searchTerm && totalMatches === 0) {
+    noResultsMessage.style.display = 'block';
   }
 
 }
@@ -252,10 +302,7 @@ document.addEventListener('DOMContentLoaded', () => {
   
   clearSearchButton.addEventListener("click", () => {
       searchInput.value = '';
-      // Pass the necessary arguments to performSearch if it expects them
-      // As per our structured guide:
-      performSearch(searchInput.value, noResultsMessage, clearSearchButton, searchInput);
-      clearSearchButton.style.display = 'none'; // Also remember to hide the button
+      performSearch();
+      clearSearchButton.style.display = 'none';
   });
-
 });
